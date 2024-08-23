c81f1bf18ab9ab479ac937bab2027eaed803cd19
adrianFD22 <adrianfd22399@gmail.com>
2024-08-23 18:26:30 +0200

    Add post: Paint

---
+++ content/sections/1_Blog/6_El Paint del Windows/index.md
@@ -0,0 +1,92 @@
+
+<style>
+    a { text-decoration: underline; }
+
+    #frame {
+        top: 1em;
+        display: block;
+        margin: auto;
+    }
+</style>
+
+# El Paint del Windows
+
+Estás usando el Paint y has hecho un dibujo. Al usar la herramienta <img src="bote.jpeg">, puedes rellenar toda un aréa del mismo color. Tus intenciones son colorear todo el dibujo con el mismo color (sin tener en cuenta el fondo). Por pereza, quieres usar el menor número posible de rellenamientos. La pregunta entonces es "dado un dibujo, cuál el menor número de rellenamientos necesarios para hacerlo monocromático?". Ten en cuenta que dos áreas del mismo color pasan a ser "la misma área". Puedes usar la siguiente interfaz^[La interfaz tiene algunos comportamientos extraños. El más evidente es que no detecta qué rectángulo está encima de otro. Así que si dos rectángulos se detectan como adyacentes aún habiendo un tercero que opaca la zona en la que se tocan los dos primeros.] para dibujar rectángulos y jugar a rellenarlos.
+
+<canvas id="frame">
+    No furula en este navegador.
+</canvas>
+
+Como siempre, aprovecho para agradecer a las personas que me han escuchado/aguantado en lo relativo a este problema. Gracias a Diego, Perete, Fer, Miguel y a mi madre!
+
+## "Formalización" del problema
+
+Creo que la forma más sencilla de modelizar este problema es, una vez más, con grafos. Consideremos el grafo cuyos vértices son las distintas áreas de colores y las aristas vienen dadas por la adyacencia de estas, es decir, dos áreas (vértices) son adyacentes si comparten un borde (compartir una esquinita no cuenta). Esto es lo que se conoce como el [grafo dual](https://es.wikipedia.org/wiki/Grafo_dual). Consideremos también la [coloración](https://es.wikipedia.org/wiki/Coloraci%C3%B3n_de_grafos) dada por el dibujo, una función que asigna a cada vértice su color.
+
+En estos términos, un rellenamiento consiste en cambiar el color de un vértice e identificar dicho vértice con sus vecinos que sean del nuevo color. Esto asegura que la nueva coloración sigue siendo correcta (vértices adyacentes tienen distinto color). De esta forma, con sucesivos rellenamientos, iremos simplificando el grafo poco a poco haciendo que el número de vértices vaya decreciendo. Por ejemplo, si
+<a onclick="loadDrawing(0); window.scrollTo(0, 0);">aquí</a>
+rellenamos el vértice del medio de azul, el grafo pasará de tener 5 vértices (3 rojos y 2 azules) a solo 3 (2 rojos y 1 azul). De esta forma, habremos acabado cuando solo quede un vértice.
+
+Aunque no lo he dicho explícitamente, notar que distintas formas de rellenar un grafo pueden resolverlo en un distinto número de rellenamientos. Esto valida la definición del número mínimo de rellenamientos necesarios para resolver un grafo $G$ coloreado según $c$. Denotaremos a este número como $r(G,c)$ o simplemente $r(G)$, obviando la coloración (classic maths).
+
+A partir de ahora voy a asumir que el grafo es conexo sin perder generalidad, ya que $r(G_1 + G_2) = r(G_1) + r(G_2)$.
+
+## Cotas inferiores
+
+Una primera observación es que si tenemos $n$ colores, vamos a necesitar al menos $n-1$ rellenamientos pues con cada rellenamiento eliminamos a lo sumo un color y queremos que el grafo final tenga solo un color, es decir, $n-1 \leq r(G)$. Esta cota es muy simplista pero por ejemplo confirma que
+<a onclick="loadDrawing(3); window.scrollTo(0, 0);">esto</a>
+tiene número de rellenamiento $r(G) = 3$.
+
+Construyamos otra cota algo mejor. Dado un vértice $v \in G$, consideramos la [excentricidad](https://es.wikipedia.org/wiki/Distancia_(teor%C3%ADa_de_grafos)) del vértice $v$ definida como la mayor distancia desde $v$ a otro vértice de $G$, esto es, $e(v) := \max_{u \in G} d(v,u)$. La excentricidad de un vértice mide cómo de céntrico es un vértice y, en particular, una excentricidad grande indica que hay un vértice muy lejano a $v$. Diremos entonces que un vértice que tenga excentricidad mínima será un centro del grafo (notar que puede haber varios centros) y llamaremos radio, denotado por $rad(G)$, a su excentricidad, la excentricidad mínima. Ya podemos enunciar la siguiente cota, que sigue siendo un poco caca a veces ya que solo depende del grafo y no de la coloración:
+
+<u>**Teorema:**</u> $rad(G) \leq r(G)$.
+
+<u>Demostración:</u> Vamos a ver que con cada rellenamiento decrece el radio a lo sumo en $1$, deduciéndose que hacen falta al menos tantos rellenamientos como $rad(G)$. Sea $G^\prime$ el grafo $G$ después de rellenar el vértice $v \in G$, es decir, identificarlo con algunos de sus vecinos $v_1,v_2,\ldots,v_s$. Supongamos que $rad(G^\prime) \leq rad(G) - 2$ para llegar a un absurdo. Sea $u \in G^\prime$ un centro, considerar un camino de tamaño mínimo $uPv = u, u^\prime, \ldots, v$. Estudiemos la excentricidad de $u^\prime$ en $G$. Dado un vértice cualquiera $w \in G$, la distancia a $u$ en $G^\prime$ es menor o igual a $rad(G^\prime) \leq rad(G) - 2$. Si hay un camino mínimo de $u$ a $w$ que no pasa por ninguno de los vértices $v, v_1, v_2 \ldots, v_s$, entonces $d_G(u^\prime, w) \leq d_G(u,w) + 1 \leq rad(G) - 1$. Para un camino mínimo que pasa por alguno de los $v_i$, este pasa a lo sumo por $3$ de estos (pues el diámetro del grafo inducido $G[v,v_1,v_2,\ldots,v_s]$ es $2$), con lo que tenemos que $d_G(u^\prime, w) \leq d_{G^\prime}(u^\prime, w) + 2 = d_{G^\prime}(u, w) - 1 + 2 \leq rad(G) - 1$. Esto contradice la definición de radio y el resultado se sigue.
+
+Q.E.D
+
+Por ejemplo, deducimos que
+<a onclick="loadDrawing(3); window.scrollTo(0, 0);">esto</a>
+se rellena con $3$ colores de forma óptima. Pero, en general, esta cota no representa el número mínimo de rellenamientos (véase
+<a onclick="loadDrawing(8); window.scrollTo(0, 0);">esto</a> como ejemplo).
+
+Esta cota funciona relativamente bien para pocos colores, pero para muchos, es una xufa.
+
+## El caso de 2 colores
+
+Los dibujos que solo usan 2 colores son los grafos bipartitos, los cuales tienen muchas propiedades muy satisfactorias de estudiar.
+
+<u>**Corolario:**</u> Para un dibujo que usa solo dos colores, $r(G) = rad(G)$.
+
+<u>Demostración:</u> El algoritmo es el siguiente: toma un vértice central y alternadamente rellénalo de un color y del otro. Observando que tras $n$ iteraciones se habrán rellenado todos los vértices a distancia $n$ o menor, el algoritmo necesita $rad(G)$ rellenamientos y, por el teorema anterior, es óptimo.
+
+En particular,
+<a onclick="loadChess(6,6); window.scrollTo(0, 0);">esto</a>
+se rellena de forma óptima con $6$ rellenamientos, pues es su radio.
+
+## Algunos dibujos con los que jugar
+
+<a onclick="loadDrawing(0); window.scrollTo(0,0);">Oruga 1</a>
+<a onclick="loadDrawing(1); window.scrollTo(0,0);">Oruga 2</a>
+<a onclick="loadDrawing(2); window.scrollTo(0,0);">Oruga 3</a>
+
+<a onclick="loadChess(3,3); window.scrollTo(0,0);">Ajedrez 3x3</a>
+<a onclick="loadChess(4,4); window.scrollTo(0,0);">Ajedrez 4x4</a>
+<a onclick="loadChess(5,5); window.scrollTo(0,0);">Ajedrez 5x5</a>
+<a onclick="loadChess(6,6); window.scrollTo(0,0);">Ajedrez 6x6</a>
+
+<a onclick="loadDrawing(3); window.scrollTo(0,0);">Cuatro colores</a>
+<a onclick="loadDrawing(4); window.scrollTo(0,0);">Lila</a>
+<a onclick="loadDrawing(5); window.scrollTo(0,0);">No conexo</a>
+<a onclick="loadDrawing(6); window.scrollTo(0,0);">Cubismo</a>
+<a onclick="loadDrawing(7); window.scrollTo(0,0);">Ciclo</a>
+<a onclick="loadDrawing(8); window.scrollTo(0,0);">Ocho</a>
+
+## Notas
+- La cota del teorema solo tiene en cuenta la estructura del grafo y no cómo está coloreado. ¿Hay alguna forma de dar una cota que aúne ambas cosas?
+- La cota dada por el teorema dice que tras identificar un subgrafo de diámetro $2$ de $G$, el radio de $G$ decrece a lo sumo en $1$. No sé si este resultado se puede generalizar...
+- He estudiado este problema en todo momento sin usar que hablo de grafos planos. No sé si hay alguna propiedad de esta familia como el Teorema de los 4 colores que pueda usarse para resolver el problema u obtener mejores cotas.
+- ¿Hay alguna cosa requetealgebraica que se le pueda asociar a $(G,c)$ para dar mejores cotas o caracterizar $r(G)$?
+- Otra cosa "interesante" que se puede plantear es un juego para dos jugadores: por turnos se colorea una de las zonas de modo que decrezca el número de vértices al menos en uno. Pierde quién obtiene en su turno el grafo con un solo vértice. Véase el juego [Col](https://es.wikipedia.org/wiki/Col_(juego)) de Conway que presenta cierta relación.
+
+<script src="paint.js"></script>
---
+++ content/sections/1_Blog/6_El Paint del Windows/paint.js
@@ -0,0 +1,388 @@
+
+const paint = new Image();
+paint.src = "paint.png";
+
+const drawings = [
+    [    // Oruga 1
+        [ [ 104.16668701171875, 143.4499969482422, 91, 79 ], [ 179.16668701171875, 136.4499969482422, 61, 90 ], [ 234.16668701171875, 146.4499969482422, 104, 81 ], [ 326.16668701171875, 138.4499969482422, 60, 85 ], [ 376.16668701171875, 157.4499969482422, 69, 59 ] ],
+        [ 7, 10, 7, 10, 7],
+    ],
+
+    [   // Oruga 2
+        [ [ 112.83331298828125, 144.6999969482422, 63, 77 ], [ 172.83331298828125, 146.6999969482422, 66, 77 ], [ 235.83331298828125, 148.6999969482422, 62, 72 ], [ 292.83331298828125, 149.1999969482422, 59, 71 ], [ 340.83331298828125, 150.1999969482422, 45, 70 ], [ 381.83331298828125, 145.1999969482422, 66, 82 ] ],
+        [ 7, 8, 7, 10, 8, 10 ]
+    ],
+
+    [   // Oruga 3
+        [ [ 97.83331298828125, 154.6999969482422, 69, 87 ], [ 165.83331298828125, 157.1999969482422, 51, 81 ], [ 213.83331298828125, 149.1999969482422, 66, 84 ], [ 271.83331298828125, 164.1999969482422, 72, 73 ], [ 339.83331298828125, 157.1999969482422, 62, 62 ] ],
+        [ 10, 7, 8, 10, 7 ]
+    ],
+
+    [   // 4 colores
+        [ [ 109.83331298828125, 164.6999969482422, 74, 56 ], [ 179.83331298828125, 163.1999969482422, 67, 58 ], [ 245.83331298828125, 163.1999969482422, 72, 57 ], [ 313.83331298828125, 167.1999969482422, 79, 54 ], [ 209.83331298828125, 119.19999694824219, 68, 58 ], [ 217.83331298828125, 211.1999969482422, 66, 64 ] ],
+        [ 7, 9, 8, 7, 10, 10 ]
+    ],
+
+    [   // Lila
+        [ [ 142.16668701171875, 114.11666870117188, 57, 42 ], [ 169.16668701171875, 150.11666870117188, 23, 58 ], [ 163.16668701171875, 200.11666870117188, 205, 38 ], [ 171.16668701171875, 234.11666870117188, 23, 80 ], [ 209.16668701171875, 229.11666870117188, 20, 56 ], [ 313.16668701171875, 230.11666870117188, 14, 55 ], [ 338.16668701171875, 235.11666870117188, 24, 80 ], [ 358.16668701171875, 200.11666870117188, 48, 10 ], [ 109.16668701171875, 130.11666870117188, 55, 18 ], [ 101.16668701171875, 129.11666870117188, 15, 16 ], [ 149.16668701171875, 122.11666870117188, 10, 6 ], [ 171.16668701171875, 106.11666870117188, 40, 21 ], [ 171.16668701171875, 176.11666870117188, 23, 20 ], [ 165.16668701171875, 186.11666870117188, 34, 4 ], [ 130.83331298828125, 149.6999969482422, 14, 6 ] ],
+        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 11, 0, 7 ]
+    ],
+
+    [   // No conexo
+        [ [ 130.83331298828125, 106.69999694824219, 54, 72 ], [ 132.83331298828125, 170.1999969482422, 55, 68 ], [ 133.83331298828125, 228.1999969482422, 52, 62 ], [ 292.83331298828125, 124.69999694824219, 57, 67 ], [ 293.83331298828125, 182.1999969482422, 53, 68 ] ],
+        [ 3, 9, 10, 10, 5 ]
+    ],
+
+    [   // Troncho
+        [ [ 141.83331298828125, 117.69999694824219, 69, 45 ], [ 205.83331298828125, 111.19999694824219, 72, 57 ], [ 250.83331298828125, 160.1999969482422, 36, 62 ], [ 110.83331298828125, 106.69999694824219, 39, 18 ], [ 119.83331298828125, 136.6999969482422, 35, 7 ], [ 123.83331298828125, 154.6999969482422, 29, 9 ], [ 266.83331298828125, 89.69999694824219, 38, 33 ], [ 273.83331298828125, 148.6999969482422, 29, 6 ], [ 239.83331298828125, 203.6999969482422, 22, 47 ], [ 269.83331298828125, 207.6999969482422, 4, 40 ], [ 281.83331298828125, 205.6999969482422, 14, 39 ] ],
+        [ 10, 3, 10, 7, 7, 7, 7, 7, 7, 7, 7 ]
+    ],
+
+    [   // Ciclo
+        [ [ 192.5, 104.11666870117188, 119, 45 ], [ 174.5, 125.11666870117188, 23, 87 ], [ 175.5, 202.11666870117188, 23, 50 ], [ 178.5, 245.11666870117188, 136, 27 ], [ 303.5, 193.11666870117188, 33, 57 ], [ 296.5, 121.11666870117188, 37, 78 ] ],
+        [ 10, 7, 8, 10, 7, 8 ]
+    ],
+
+    [   // Ocho
+        [ [ 196.5, 110.11666870117188, 107, 25 ], [ 293.5, 122.11666870117188, 25, 72 ], [ 189.5, 120.11666870117188, 22, 65 ], [ 181.5, 180.11666870117188, 40, 23 ], [ 275.5, 180.11666870117188, 48, 21 ], [ 221.5, 181.11666870117188, 58, 13 ], [ 193.5, 198.11666870117188, 17, 49 ], [ 302.5, 195.11666870117188, 18, 61 ], [ 204.5, 241.11666870117188, 113, 13 ] ],
+        [ 7, 10, 10, 8, 8, 7, 7, 7, 10 ]
+    ],
+];
+
+function draw() {
+    // Clean the canvas. js is damn crazy
+    canvas.width = canvas.width;
+    canvas.height = canvas.height;
+
+    // Draw interface
+    ctx.drawImage(paint, 0, 0, canvas.width, canvas.height);
+
+    // Draw selected tool
+    ctx.strokeStyle = "#A1A49E";
+    ctx.lineWidth = "3";
+    switch (sel_tool) {
+        case "rectangle":
+            ctx.rect(5,  53,  25, 25);
+            break;
+
+        case "bucket":
+            ctx.rect(31,  53,  25, 25);
+            break;
+
+        case "eraser":
+            ctx.rect(5,  79,  25, 25);
+            break;
+    }
+
+    // For each rectangle
+    for (let i=0; i < rect_list.length; i++) {
+        curr_rect = rect_list[i];
+
+        //ctx.globalAlpha = 0.5;
+        ctx.fillStyle = colors[rect_colors[i]];
+        ctx.fillRect(curr_rect[0], curr_rect[1], curr_rect[2], curr_rect[3]);
+        //ctx.globalAlpha = 1.0;       // Draw it
+    }
+
+    // Draw available colors
+    for (let i=0; i < colors.length/2; i++) {
+        // Row 1
+        ctx.fillStyle = "#A1A49E";
+        ctx.fillRect(36 + 16*i, 343, 14, 14);
+
+        ctx.fillStyle = colors[i];
+        ctx.fillRect(37 + 16*i, 344, 14, 14);
+
+        // Row 2
+        ctx.fillStyle = "#A1A49E";
+        ctx.fillRect(36 + 16*i, 360, 14, 14);
+
+        ctx.fillStyle = colors[i+colors.length/2];
+        ctx.fillRect(37 + 16*i, 361, 14, 14);
+    }
+
+    // Draw selected rectangle
+    ctx.fillStyle = colors[sel_color];
+    ctx.fillRect(10,350,12,12);
+
+    // Draw number of moves
+    if (sel_tool == "bucket") {
+        ctx.fillStyle = "black";
+        ctx.font = "15px Arial";
+        ctx.fillText(n_moves.toString(), 15,283);
+        //[15,280,20,30]        // el texto
+    }
+
+    // Draw new drawing button
+    //[5,30,15,15]
+    ctx.fillStyle = "black";
+    ctx.font = "12px Arial";
+    ctx.fillText("Nuevo", 7,42);
+
+    ctx.stroke();
+}
+
+// Given indices in the square_list, return true or false depending if the rectangle collide
+function rect_collide(i,j) {
+    rect1 = rect_list[i];
+    rect2 = rect_list[j];
+
+    // Check if one rectangle is completely at left of the other
+    if (rect1[0] + rect1[2] < rect2[0] || rect2[0] + rect2[2] < rect1[0]) {
+        return false;
+    }
+
+    // Check if one rectangle is completely above of the other
+    if (rect1[1] + rect1[3] < rect2[1] || rect2[1] + rect2[3] < rect1[1] ) {
+        return false;
+    }
+
+    // Check if rectangles share only one point (this is how the dual graph works)
+    if (rect1[0] + rect1[2] == rect2[0] || rect2[0] + rect2[2] == rect1[0]) {
+        if (rect1[1] + rect1[3] == rect2[1] || rect1[1] == rect2[1] + rect2[3]) {
+            return false;
+        }
+    }
+
+    // The alternative is both colliding
+    return true;
+}
+
+function isPointInsideRect(x,y,rect) {
+    return x >= rect[0] && x <= rect[0] + rect[2] && y >= rect[1] && y <= rect[1] + rect[3];
+}
+
+function drawRect(x,y) {
+    if (pending_rect.length == 0) {     // Ugly code
+        ctx.strokeStyle = "#000000";
+        ctx.beginPath();
+        ctx.arc(x, y, 4, 0, 2 * Math.PI);
+        ctx.stroke();
+
+        pending_rect = [x,y];
+    }
+    else {
+        // Draw rectangle
+        rect_list.push([Math.min(x,pending_rect[0]), Math.min(y, pending_rect[1]), Math.abs(x-pending_rect[0]), Math.abs(y-pending_rect[1])]);
+        rect_colors.push(sel_color);
+
+        pending_rect = [];
+        draw();
+    }
+}
+
+function paintRect(x,y) {
+    // Identify rectangle to paint
+    sel_rect = -1;
+    for (let i=rect_list.length-1; i>=0; i--) {
+        if (isPointInsideRect(x,y,rect_list[i])) {
+            sel_rect = i;
+            break;
+        }
+    }
+
+    if (sel_rect == -1) {
+        return;
+    }
+
+    // Color the rectangle
+    n_moves++;
+    sel_rect_color = rect_colors[sel_rect];
+    rect_stack = [sel_rect];
+    visited = [];
+
+    while (rect_stack.length > 0) {
+        curr_rect = rect_stack[0];
+        rect_stack.shift();
+        visited.push(curr_rect);
+
+        // Change color to current rect
+        rect_colors[curr_rect] = sel_color;
+
+        // Get neighbors
+        for (let i=0; i<rect_list.length; i++) {
+            if (adjacency_matrix[curr_rect][i] == 1 && rect_colors[i] == sel_rect_color && ! visited.includes(i) && ! rect_stack.includes(i)) {
+                rect_stack.push(i);
+            }
+        }
+    }
+}
+
+function selectRect(x,y) {
+    for (let i=rect_list.length-1; i>=0; i--) {
+        curr_rect = rect_list[i];
+
+        if (isPointInsideRect(x,y,curr_rect)) {
+        //if (x >= curr_rect[0] && x <= curr_rect[0] + curr_rect[2] && y >= curr_rect[1] && y <= curr_rect[1] + curr_rect[3]) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function handleClick(event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    // If click is inside the frame
+    if (isPointInsideRect(x,y, [63,52,430,280])) {
+    //if (x >= 63 && x<= 493 && y >= 52 && y <= 337) {
+        switch (sel_tool) {
+            case "rectangle":
+                drawRect(x,y);
+                break;
+
+            case "bucket":
+                paintRect(x,y);
+                draw();
+                break;
+
+            case "eraser":
+                sel_rect = selectRect(x,y);
+                if (sel_rect != -1) {
+                    rect_list.splice(sel_rect, 1);
+                    rect_colors.splice(sel_rect,1);
+                    draw();
+                }
+                break;
+        }
+        return;
+    }
+
+    for (let i=0; i < colors.length/2; i++) {
+        if (isPointInsideRect(x,y,[37 + 16*i, 344, 14, 14])) {
+            sel_color = i;
+            break;
+        }
+
+        if (isPointInsideRect(x,y,[37 + 16*i, 361, 14, 14])) {
+            sel_color = i + colors.length/2;
+            break;
+        }
+    }
+
+    // If click is in rectangle tool
+    if (isPointInsideRect(x,y,[5,53,25,25])) {
+    //if (x >= 5 && x <= 30 && y >= 53 && y <= 78) {
+        if (sel_tool == "bucket") {
+            rect_colors = saved_colors.slice();
+        }
+        sel_tool = "rectangle";
+    }
+
+
+    else if (isPointInsideRect(x,y,[31,53,25,25])) {
+    //else if (x >= 31 && x <= 56 && y >= 53 && y <= 78) {
+        sel_tool = "bucket";
+        n_moves = 0;
+
+        // Construct colored graph
+        saved_colors = rect_colors.slice();
+
+        adjacency_matrix = Array(rect_list.length).fill().map(() => Array(rect_list.length).fill(0));
+        for (let i=0; i<rect_list.length; i++) {
+            for (let j=i+1; j<rect_list.length; j++) {
+                if (rect_collide(i,j)) {
+                    adjacency_matrix[i][j] = 1;
+                    adjacency_matrix[j][i] = 1;
+                }
+            }
+        }
+    }
+
+    else if (isPointInsideRect(x,y,[5,79,25,25])) {
+    //else if (x >= 5 && x <= 30 && y >= 79 && y <= 104) {
+        if (sel_tool == "bucket") {
+            rect_colors = saved_colors.slice();
+        }
+        sel_tool = "eraser";
+    }
+
+    // If click is in New drawing button
+    //[5,30,15,15]
+    else if (isPointInsideRect(x,y,[4,30,43,15])) {
+        rect_list = [];
+        rect_colors = [];
+        sel_tool = "rectangle";
+    }
+
+    pending_rect = [];
+    draw();
+}
+
+function loadDrawing(index) {
+    rect_list = drawings[index][0].slice();
+    rect_colors = drawings[index][1].slice();
+    //saved_colors = drawings[index][1].slice();
+    sel_tool = "rectangle";
+    pending_rect = [];
+    draw();
+}
+
+function loadChess(rows,cols) {
+    rect_list = [];
+    rect_colors = [];
+    pending_rect = [];
+    sel_tool = "rectangle";
+    const size = 40;
+
+    for (let i=0; i<rows; i++) {
+        for (let j=0; j<cols; j++) {
+            rect_list.push([110+size*j, 80+size*i,size,size]);
+            if (i % 2 == j % 2) {
+                rect_colors.push(0);
+            } else {
+                rect_colors.push(6);
+            }
+            //[130,100,50,50],
+        }
+    }
+
+    draw();
+}
+
+
+//---------------------
+//        Main
+//---------------------
+
+const canvas = document.getElementById("frame");
+canvas.width  = 500;
+canvas.height = 382;
+
+const ctx = canvas.getContext("2d");
+
+let rect_list = [];
+let rect_colors = [];
+let saved_colors = [];
+let n_moves = 0;
+
+let colors = [
+    "#000000",
+    "#9C5A3C",
+    "#FF7E00",
+    "#22B14C",
+    "#2f3699",
+    "#6F3198",
+
+    "#B4B4B4",
+    "#ED1C24",
+    "#FFF200",
+    "#A8E61D",
+    "#00B7EF",
+    "#FFA3B1"
+]
+
+let sel_tool = "rectangle";      // Mode can be "draw", "fill" or "erase"
+let sel_color = 0;
+let pending_rect = [];
+
+paint.addEventListener("load", () => {
+    draw();
+});
+
+canvas.addEventListener('mousedown', function(e) {
+    handleClick(e);
+});
