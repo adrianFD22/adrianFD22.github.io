3f87d15af310e68e339611429fda1cd207bfebe6
adrianFD22 <adrianfd22399@gmail.com>
2024-08-31 18:58:55 +0200

    Add alternative interface to treblecross

--- content/sections/1_Blog/7_Cross, Treble cross/index.md
+++ content/sections/1_Blog/7_Cross, Treble cross/index.md
@@ -2,6 +2,8 @@
 # Cross, Treble Cross
 
 <style>
+    a { text-decoration: underline; }
+
     #board {
         display: block;
         margin: auto;
@@ -30,16 +32,26 @@ James Bond juega con el Dr. No a un juego para decidir si el Dr No le revela su 
 <table style="">
     <tr>
         <td> <label>Tamaño</label> </td>
-        <td> <input style="width: 3em" id="n_squares" type="number" min="1" max="30" value=15> </td>
+        <td> <input style="width: 3em" id="n_squares" type="number" min="1" max="30" value=14> </td>
     </tr>
     <tr>
         <td> <button type="button" onclick="generateBoard(); draw();">Generar tablero</button><br> </td>
     </tr>
 </table>
 
+## Un poco de estrategia
+
+Hay movimientos que significan perder inmediatamente. Si dibujamos una cruz pegada a otra, en el turno siguiente nuestro oponente hará 3 en raya. Lo mismo pasa con dibujar una cruz dejando solo una casilla de separación con la más próxima, pues nuestro oponente jugará entre ambas y ganará. De esta forma, podemos pensar que cada cruz "bloquea" las casillas que se encuentran a 1 ó 2 de distancia, pues suponen perder inmediatamente. El juego se convierte así en uno de ir eliminando casillas hasta que no quede ninguna (juega en la interfaz <a id="switch_button" onclick="switchMode(this); window.scrollTo(0,0);">pro</a>).
+
+Con este enfoque, podemos ver que el primer jugador siempre se puede llevar la victoria cuando juega en un tablero impar. Esto es porque puede empezar jugando en el centro, lo que dividirá el tablero en dos tableros iguales más pequeñitos. De esta forma, por cada movimiento que haga su oponente en un tablero, este podrá responder jugando el mismo movimiento pero en el tablero opuesto. Esta estrategia de jugar en "espejo" es muy habitual en este tipo de juegos.
+
+Hasta donde yo sé, no se conoce una estrategia ganadora para un tablero par 🤷‍♂️.
+
 ## Notas
 - Este juego se llama [Treblecross](https://en.wikipedia.org/wiki/Treblecross) y es del capítulo "Take and break" de [Winning Ways for your Mathematical Plays](https://es.wikipedia.org/wiki/Winning_Ways_for_your_Mathematical_Plays). Su notación [octal](https://es.wikipedia.org/wiki/Juego_octal) es 007 (ver esta [explicación](https://math.stackexchange.com/questions/4283174/why-do-we-say-treblecross-octal-game-notation-is-007)). Este juego no aparece en ninguna película de James Bond, al menos que yo sepa.
 - En el momento de escribir esto, agosto de 2024, no se conoce si este juego es periódico o no. Algunos (muchos) valores de su sucesión de NIM están calculados en las [tablas de juegos octales](http://wwwhomes.uni-bielefeld.de/achim/octal.html) de Achim Flammenkamp.
-- Notar que [Dawson's chess](http://gamescrafters.berkeley.edu/games.php?game=dawsonschess) en modo misère es el mismo juego que Treblecross (en modo normal).
+- Por si quieres
+<a onclick="nimMode(); window.scrollTo(0,0);">ver</a>
+los nim values mientras juegas.
 
 <script src="007.js"></script>
--- sections/Blog/Cross, Treble cross/007.js
+++ sections/Blog/Cross, Treble cross/007.js
@@ -1,4 +1,9 @@
 
+// I started writint a simple interface and I started to
+// patch it in order to include more things (alternative interface
+// and displaying nim values). Sorry to myself if I eventually have to
+// debug this 💩.
+
 //---------------------
 //      Parameters
 //---------------------
@@ -7,6 +12,7 @@ const square_size = 40;
 const square_margin = 5;
 const square_border = 1;
 const cross_width = 7;
+const nim_sequence = [0,1,1,1,2,2,0,3,3,1,1,1,0,4,3,3,3,2,2,2,4,4,0,5,5,2,2,2,3,3,0,5,0,1,1,1,3,3,3,5,6,4,4,1,0,5,5,6,6,2,7,7,7,8,0,1,9,2,7,2,3,3,3,9,0,5,4,4,8,6,6,2,7,1,1,1,0,5,5,9,3,1,8,2,8,5,0,1,1,12,2,2,7,3,3,9,4,4,0,11,3,];
 
 
 //---------------------
@@ -18,15 +24,19 @@ function draw() {
     canvas.width = board.length*square_size;
     canvas.height = square_size;
 
+    curr_length = 0;
+
     // Draw board
     for (let i=0; i<board.length; i++) {
-        ctx.fillStyle = "white";
-        ctx.fillRect(i * square_size, 0, square_size, square_size);
-        ctx.lineWidth = square_border;
-        ctx.strokeStyle = "black";
-        ctx.strokeRect(i * square_size, 0, square_size, square_size);
+        if (! is_pro_mode || (board[i] == 0)) {
+            ctx.fillStyle = "white";
+            ctx.fillRect(i * square_size, 0, square_size, square_size);
+            ctx.lineWidth = square_border;
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(i * square_size, 0, square_size, square_size);
+        }
 
-        if (board[i] == 1) {
+        if (board[i] == 1 && ! is_pro_mode) {
             // Draw cross
             ctx.strokeStyle = "red";
             ctx.lineWidth = cross_width;
@@ -37,6 +47,31 @@ function draw() {
             ctx.lineTo(i*square_size + square_margin , square_size - square_margin);
             ctx.stroke();
         }
+
+        if (show_nim_values) {
+            if (board[i] != 0) {
+                if (curr_length != 0) {
+                    curr_nim_value = curr_length < nim_sequence.length ? nim_sequence[curr_length] : "?";
+                    ctx.fillStyle = "black";
+                    ctx.font = (square_size/2).toString().concat("px Arial");
+                    ctx.fillText(curr_nim_value, square_size*(i-1) + square_size/2, 2/3*square_size);
+                    ctx.stroke();
+                }
+
+                curr_length = 0;
+            }
+            else {
+                curr_length++;
+            }
+        }
+    }
+
+    if (curr_length != 0) {
+        curr_nim_value = curr_length < nim_sequence.length ? nim_sequence[curr_length] : "?";
+        ctx.fillStyle = "black";
+        ctx.font = (square_size/2).toString().concat("px Arial");
+        ctx.fillText(curr_nim_value, square_size*(board.length-1) + square_size/2, 2/3*square_size);
+        ctx.stroke();
     }
 
     // Print turn
@@ -50,6 +85,7 @@ function draw() {
 function generateBoard() {
     n_squares = document.getElementById("n_squares").value;
     turn = 0;
+    available_plays = n_squares;
     is_game_over = false;
     board = [];
 
@@ -62,32 +98,66 @@ function generateBoard() {
 function handleClick(event) {
     const rect = canvas.getBoundingClientRect()
     const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    //const y = event.clientY - rect.top;
 
     sel_square = Math.floor(x / square_size);
 
     // If clicked square is valid
-    if (board[sel_square] == 0 && ! is_game_over) {
+    if (! is_game_over &&
+        (! is_pro_mode && board[sel_square] != 1) ||
+        (is_pro_mode && board[sel_square] == 0)) {
         board[sel_square] = 1;
+        available_plays--;
 
-        // Check if is winning move
         counter = 0;
-        for (let i=0; i<board.length; i++) {
+        three_in_a_row = false;
+
+        for (let i=Math.max(sel_square-2, 0); i<Math.min(board.length, sel_square+3); i++) {
             if (board[i] == 1) {
                 counter++;
                 if (counter == 3) {
-                    is_game_over = true;
-                    break;
+                    three_in_a_row = true;
                 }
             }
             else {
+                if (board[i] == 0) {
+                    available_plays--;
+                    board[i] = -1;
+                }
                 counter = 0;
             }
         }
+
+        // Check if it is a winning move
+        if (is_pro_mode) {
+            is_game_over = available_plays == 0;
+        } else {
+            is_game_over = three_in_a_row;
+        }
+
         if (! is_game_over) {
             turn = (turn + 1) % 2;
         }
     }
+}
+
+function switchMode(o) {
+    is_pro_mode = ! is_pro_mode;
+    o.text = is_pro_mode ? "natural" : "pro";
+    if (! is_pro_mode) {
+        show_nim_values = false;
+    }
+    generateBoard();
+    draw();
+}
+
+function nimMode() {
+    document.getElementById("switch_button").innerHTML = "natural";
+
+    show_nim_values = true;
+    is_pro_mode = true;
+    generateBoard();
+    draw();
 }
 
 
@@ -98,6 +168,10 @@ function handleClick(event) {
 board = [];
 turn = 0;
 is_game_over = false;
+is_pro_mode = false;
+available_plays = 0;
+nim_values = [];
+show_nim_values = false;
 
 // Get canvas
 const canvas = document.getElementById("board");
